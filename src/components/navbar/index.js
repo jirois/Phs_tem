3a64be67418f35547744a341d923825d9bf109a9
Omas Ajiri <omasajiri@gmail.com>
2022-01-23 12:34:16 +0100

    feat: setup dependencies

---
+++ src/components/navbar/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+    Nav,
+    NavbarContainer,
+    NavbarIcon
+}
+    from './navbarElements'
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavbarIcon>
+                        Ups
+                    </NavbarIcon>
+                </NavbarContainer>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar;
---
+++ src/components/navbar/navbarElements.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Nav = styled.nav`
+    background: #000;
+    height: 80px;
+    font-size: 1rem;
+    display: flex;
+    align-item: center;
+    z-index: 10;
+`
+export const NavbarContainer = styled.div`
+    width: 100%;
+    height: 100%;
+`
+
+export const NavbarIcon = styled.h3`
+    color: #fff;
+`
